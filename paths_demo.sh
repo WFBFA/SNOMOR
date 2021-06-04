b95ecd72318696f04d95c730db2dfccfe24b972a
AristideEU <57840460+AristideEU@users.noreply.github.com>
2021-06-04 22:56:48 +0200

    Update paths_demo.sh

--- paths_demo.sh
+++ paths_demo.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-RUST_LOG="trace" cargo run --manifest-path Real_case/Paths/Cargo.toml -- plow Real_case/Paths/demo/montreal/roads.json Real_case/Paths/demo/montreal/snow.lrive.json Real_case/Paths/demo/montreal/vehicles.json Real_case/Paths/demo/montreal/plow.meta.yaml ./../plow-output.json
+RUST_LOG="trace" cargo run --manifest-path Real_case/Paths/Cargo.toml -- plow Real_case/Paths/demo/montreal/roads.json Real_case/Paths/demo/montreal/snow.lrive.json Real_case/Paths/demo/montreal/vehicles.json Real_case/Paths/demo/montreal/plow.meta.yaml ./plow-output.json
